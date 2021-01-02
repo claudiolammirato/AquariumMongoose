f52a15b7215f38f887e6f4dd2f1fd769de2125fe
claudiolammirato <claudio.lammirato@gmail.com>
2021-01-02 19:57:21 +0100

    init

--- server.js
+++ server.js
@@ -18,11 +18,37 @@ connection.once("open", function() {
   console.log("MongoDB database connection established successfully");
 });
 
+app.set('view engine', 'ejs');
+
+
 app.use(express.urlencoded());
 app.use(express.json());
-app.use("/", Router);
+app.use("/api", Router);
 
+// index page
+app.get('/', function(req, res) {
+    var mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
 
+    var user = "Claudio";
+
+    res.render('pages/index', {
+        mascots: mascots,
+        tagline: tagline,
+        user: user
+    });
+});
+// about page
+app.get('/about', function(req, res) {
+    var user = "Claudio";
+    res.render('pages/about',{
+        user:user
+    });
+});
 
 
 app.listen(port, function() {
